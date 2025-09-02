e452b74da1c9eae6cf30747b67f09d6900f24b2d
Ayushh <ayushmishra0228@outlook.com>
2025-09-02 18:51:04 +0530

    Fix NextAuth redirect loop by excluding signin page from middleware

--- middleware.ts
+++ middleware.ts
@@ -12,5 +12,5 @@ export default withAuth(
 )
 
 export const config = {
-  matcher: ["/((?!api/auth|_next/static|_next/image|favicon.ico|Onboard.svg).*)"],
+  matcher: ["/((?!api/auth|auth/signin|_next/static|_next/image|favicon.ico|Onboard.svg).*)"],
 }
